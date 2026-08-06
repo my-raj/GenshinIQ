900947280178f66f69270e10893989b895309475
my-raj <rajendm@purdue.edu>
2026-08-06 10:33:47 +0530

    Scoring teams

--- src/main/java/com/mytri/genshin_calculator/TeamGeneratorService.java
+++ src/main/java/com/mytri/genshin_calculator/TeamGeneratorService.java
@@ -3,6 +3,7 @@ package com.mytri.genshin_calculator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,11 +14,44 @@ public class TeamGeneratorService {
     private CharacterScoreService characterScoreService;
     public List<List<GenshinCharacter>> generateTeams(List<GenshinCharacter> allCharacters, List<String> bossWeaknesses, List<String> bossImmunities) {
         Map<GenshinCharacter, Double> scores = new HashMap<>();
+        List<GenshinCharacter> mainDpsList = new ArrayList<>();
+        List<GenshinCharacter> healerList = new ArrayList<>();
+        List<GenshinCharacter> supportList = new ArrayList<>();
+        List<List<GenshinCharacter>> teams = new ArrayList<>();
         for (GenshinCharacter character : allCharacters) {
             double score = characterScoreService.scoreCharacter(character, bossWeaknesses, bossImmunities);
             scores.put(character, score);
+            if (RoleLookup.getRole(character.getName()) == CharacterRoles.MAIN_DPS) {
+                mainDpsList.add(character);
+            } else if (RoleLookup.getRole(character.getName()) == CharacterRoles.HEALER ||
+                    RoleLookup.getRole(character.getName()) == CharacterRoles.SHIELDER) {
+                healerList.add(character);
+            } else {
+                supportList.add(character);
+            }
         }
         allCharacters.sort((a, b) -> Double.compare(scores.get(b), scores.get(a)));
-        return List.of();
+        mainDpsList.sort((a, b) -> Double.compare(scores.get(b), scores.get(a)));
+        healerList.sort((a, b) -> Double.compare(scores.get(b), scores.get(a)));
+        supportList.sort((a, b) -> Double.compare(scores.get(b), scores.get(a)));
+
+        for (int i = 0; i < 3; i++) {
+            List<GenshinCharacter> team = new ArrayList<>();
+            if (i >= mainDpsList.size()) break;
+            team.add(mainDpsList.get(i));
+            if (!healerList.isEmpty()) team.add(healerList.get(0));
+            int k = 0;
+            for (GenshinCharacter character : supportList) {
+                if (k == 2) {
+                    break;
+                }
+                if (!team.contains(character)) {
+                    team.add(character);
+                    k++;
+                }
+            }
+            teams.add(team);
+        }
+        return teams;
     }
 }
